320fc0b916d24a4672990474155211c1e0eb82c0
adairon <adairon@gmail.com>
2020-04-01 19:06:36 +0200

    design page de profile avec bootstrap et CSS

--- assets/pages/ProfilePage.jsx
+++ assets/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ProfilesAPI from "../services/profilesAPI";
+import { Helmet } from "react-helmet";
 
 const ProfilePage = props => {
   // on récupère l'identifiant du profil concerné :
@@ -18,7 +19,8 @@ const ProfilePage = props => {
     linkUrl: "",
     instrument: "",
     localization: "",
-    style: ""
+    style: "",
+    user: ""
   });
 
   //On récupère les données du profil
@@ -36,7 +38,8 @@ const ProfilePage = props => {
         linkUrl,
         instrument,
         localization,
-        style
+        style,
+        user
       } = await ProfilesAPI.findOne(id);
       setProfile({
         lastName,
@@ -48,7 +51,8 @@ const ProfilePage = props => {
         linkUrl,
         instrument,
         localization,
-        style
+        style,
+        user
       });
       setLoading(false);
     } catch (error) {
@@ -64,45 +68,69 @@ const ProfilePage = props => {
 
   return (
     <>
-      <div className="container border border-dark my-2">
-        <div className="row justify-content-center">
-          <figure className="col-6 profile_pic p-1 my-2">
-            <img className="img-thumbnail" src={profile.pictureUrl} alt="" />
-          </figure>
-          <div className="col-6 profile_info p-1 border border-primary my-2">
-            <h1>
-              {profile.firstName}{" "}
-              {profile.type.name === "musicien.ne" && profile.lastName}
-            </h1>
-            <p> {profile.type.name} </p>
-            <div className="style p-1 my-2 border border-secondary">
-              <h3>Style de musique principal :</h3>
-              <p> {profile.style.name} </p>
-            </div>
-            {profile.type.name === "musicien.ne" && (
-              <div className="instrus p-1 my-2 border border-secondary">
-                <h3>Instrument joué : </h3>
-                <p> {profile.instrument.name} </p>
+      <Helmet>
+        <title>Zicos : profil de {profile.firstName} </title>
+      </Helmet>
+      <div className="bg-dark py-2">
+      
+        <div className="container profile border rounded py-2 bg-light">
+          <div className="row justify-content-center">
+
+            <figure className="col-lg-6 col-md-12 col-sm-12 profile_pic p-1 my-2 d-flex">
+              <img className="img-thumbnail profile_picture" src={profile.pictureUrl} alt="" />
+            </figure>
+
+            <div className="col-lg-6 col-md-12 col-sm-12 profile_info p-1 my-2">
+            <div className="alert alert alert-dark mx-2">
+                <h1 className="profile_name">
+                  {profile.firstName}{" "}
+                  {profile.type.name === "musicien.ne" && profile.lastName}
+                </h1>
+
+                <p className="profile_type text-center"> {profile.type.name} </p>
               </div>
-            )}
-            <div className="localization p-1 my-2 border border-secondary">
-              <h3>Région :</h3>
-              <ul>
-                <li>{profile.localization.region}</li>
-              </ul>
+
+              {profile.type.name === "musicien.ne" && (
+                <div className="instrus p-1 m-2 alert alert-secondary">
+                  <h3>Instrument : </h3>
+                  <span className="badge badge-dark">
+                    {profile.instrument.name} 
+                  </span>
+                </div>
+              )}
+
+              <div className="localization p-1 m-2 alert alert-secondary">
+                <h3>Région :</h3>
+                <span className="badge badge-dark">
+                  {profile.localization.region}
+                </span>
+              </div>
+              <div className="style p-1 m-2 alert alert-secondary">
+                <h3>Style de musique principal :</h3>
+                <span className="badge badge-dark">
+                  {profile.style.name}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="bio p-1 my-2 border border-primary">
-          <h3>Présentation : </h3>
-          <p> {profile.biography} </p>
-        </div>
-        <div className="link p-1 my-2 border border-primary">
-          <h3>Liens :</h3>
-          <p>
-            {" "}
-            <a href={profile.linkUrl}> {profile.linkUrl} </a>{" "}
-          </p>
+          <div className="profile_bio p-1 my-2 border border-light rounded">
+            <h3 className="profile_subtitle" >Présentation : </h3>
+            <p> {profile.biography} </p>
+          </div>
+          <div className="profile_link p-1 my-2 border border-light rounded">
+            <h3 className="profile_subtitle" >Liens :</h3>
+            <p className="profile_link_url">
+              <a target="_blank" href={profile.linkUrl}> {profile.linkUrl} </a>{" "}
+            </p>
+          </div>
+          <div className="profil_contact d-flex my-3">
+            <button className="btn btn-primary m-auto" type="button">
+                <a className="text-white" href={"mailto:" + profile.user.email}> 
+                  Ce profil vous intéresse ? <br/>
+                  Envoyer un mail
+                </a>
+            </button>
+          </div>
         </div>
       </div>
     </>
