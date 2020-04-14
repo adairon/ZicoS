a13ff39027515f8affcc30f69cdb05d51962bb43
adairon <adairon@gmail.com>
2020-04-14 17:59:27 +0200

    mise en place d'un cache pour la page des profils

--- assets/js/services/profilesAPI.js
+++ assets/js/services/profilesAPI.js
@@ -1,27 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
+import Cache from "./cache";
 
-function findAll({cancelToken}) {
-    return axios
-        .get("http://localhost:8000/api/profiles", {cancelToken})
-        .then(response => response.data["hydra:member"]);
+async function findAll({ cancelToken }) {
+  const cachedProfiles = await Cache.get("profiles");
+
+  if (cachedProfiles) return cachedProfiles;
+
+  return axios
+    .get("http://localhost:8000/api/profiles", { cancelToken })
+    .then((response) => {
+      const profiles = response.data["hydra:member"];
+      Cache.set("profiles", profiles);
+      return profiles;
+    });
 }
 
 function findOne(id) {
-    return axios
-        .get("http://localhost:8000/api/profiles/" + id)
-        .then(response => response.data);
+  return axios
+    .get("http://localhost:8000/api/profiles/" + id)
+    .then((response) => response.data);
 }
 
+function deleteProfil(id) {
+    
 
-function deleteProfil(id){
-    return axios
-        .delete("http://localhost:8000/api/profiles/" + id)
+  return axios.delete("http://localhost:8000/api/profiles/" + id).then(async response => {
+    const cachedProfiles = await Cache.get("profiles")
+
+    if(cachedProfiles) {
+        Cache.set("profiles", cachedProfiles.filter(p => p.id !== id))
+    }
+    return response;
+  });
 }
-
-
 
 export default {
-    findAll,
-    delete: deleteProfil,
-    findOne,
-}
+  findAll,
+  delete: deleteProfil,
+  findOne,
+};
